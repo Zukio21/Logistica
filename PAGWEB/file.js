410ba9636b26550a3a4b7451c9a9a15bcd4a26a0
Zukio21 <119967300+Zukio21@users.noreply.github.com>
2023-01-06 14:53:38 -0500

    Add files via upload

---
+++ PAGWEB/file.js
@@ -0,0 +1,176 @@
+
+let admin;
+//let nameJava = prompt('What is name with JavaScript?',"");
+/*let a= prompt("firts number?",1);
+let b= prompt("Second number?",2);
+*/
+/*if(nameJava=="ECMAscript"){
+    alert("right");
+}else{
+    alert('wrong');
+}
+*/
+//let num = prompt("INGRESE UN NUMERO",);
+
+  /*  if(num > 0){
+        alert(1);
+    } else if( num < 0)
+        {
+            alert(-1);
+
+    }else{
+
+        alert(0);
+    }
+*/
+//let result =(a + b < 4)? 'below':'Over';
+
+/*let login= prompt("INGRESE  SU CARGO","");
+
+let message =(login == 'Employee')? 'hello':
+(login == 'Director')? 'Greetings':
+(login== '')? 'No login':
+'';
+alert(message);
+*/
+
+/*if(age >=14 && age<=90)
+
+if(!(age>=14 && age<=90) )
+if(age> 14 || age>90)
+*/
+/*
+let begin = prompt("Who is ther?",);
+let password;
+if(begin =='admin'){
+ password = prompt('Write ure Pass',);
+    if(password =='TheMaster')
+    {
+        alert('Welcome');  
+    }
+    else if(  password =='' || password==null  ){
+        alert('Canceled');
+    }else{
+
+        alert('wrong');
+    }
+         
+}
+    else if(begin =='' || password== null)
+    {
+        alert('Canceled');
+    }  
+    else{
+        alert('I don`t know u'); 
+    } 
+    */
+/*
+    for(let num= 2; num<=10; num++){
+        if(num%2==0){
+            alert(num)
+        }
+    }
+    */
+   /*let i=0;
+   while(i<3) alert(`number ${i++}!`);
+*/
+/*
+let i = prompt("Ingrese un numero",);
+
+while(true){
+ 
+
+if(i%2==1){
+    alert(i);
+    for(i==2; i-=2;){
+        alert(i);
+
+        if(i==3){
+           
+            i--;
+            alert(i);
+        }
+        
+    }
+    break;
+}
+else if(i=="" || i==null) {
+   break;
+}else{
+    i= prompt("Ingrese numero",);
+    
+}
+}
+*/
+/*
+let array_Browser= ["Chrome","Firefox","Sfari","Opera"];
+
+let browser = prompt("INGRESE su navegador");
+
+if( browser==="Edge"){
+    alert( "U`ve got the edge");
+    
+    }
+for(let x =0; x<=3; x++){
+  For(let y =0; y<=3; y++)  
+     if(browser===array_Browser[x]){
+        alert("Okay we support these browser too");
+        break;
+    }
+    
+     if(browser!= "Edge" || browser!= array_Browser[y]){
+        alert("Unknown");
+        break;
+    }
+    
+}
+*/
+
+
+/*
+function checkAge(age){
+
+return(age > 18) ?   true: confirm("Did parents allow yotu?");
+
+
+}
+function checkAge(age){
+
+    return(age > 18) ||   confirm("Did parents allow yotu?");
+    
+    
+    }
+checkAge(edad);
+*/
+/*
+function min(a,b){
+
+return (a<b)?  alert(a): alert(b);
+
+}
+
+min(edad, e);
+*/
+/*
+
+function pow(a,b){
+let result = a;
+for(let x=1; x<b; x++){
+    result *=a; 
+ 
+}
+return result;
+}
+let a = prompt("ingrese a:",);
+let b= prompt("ingrese b:");
+b< 1 ? alert( `Power ${b} is not supported, use a positive  integer`): alert(pow(a,b));
+*/
+
+/*let  ask = ( question,yes,no)=>{
+let validar =(confirm(question)) ? yes(): no();
+
+}
+
+ask("Do you agree?", ()=>alert("You agreed."),  () => alert("You canceled the execution."));
+
+*/
